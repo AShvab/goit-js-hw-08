3f7a33b0dcd8a4650e2c3d379aa66f1fc1a24670
AShvab <shvabovska@gmail.com>
2023-03-13 21:59:17 +0200

    add task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from "lodash.throttle";
 
+const STORAGE_KEY = "feedback-form-state";
+let formData = {};
+
+const refs = {
+    form: document.querySelector('.feedback-form'),   
+    input: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea'),
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormDataInput, 500));
+populateFormData();
+
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    if (!formData.email || !formData.message){
+       return alert('Всі поля мають бути заповнені!');
+        
+    } else {
+        console.log(formData);
+        evt.currentTarget.reset();
+        localStorage.removeItem(STORAGE_KEY);
+        formData.email = '';
+        formData.message = '';
+    }
+};
+
+function onFormDataInput(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+function populateFormData() {
+    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY))
+
+    if (savedMessage) {
+        formData = savedMessage;
+    
+        for (const key in formData) {
+            refs.form.elements[key].value = formData[key];
+        }
+    }
+};
